627ccc0ac5bc24a2443bf491b0665765ccea3107
Matheus Araujo <matheus2001sousa@hotmail.com>
2024-10-17 14:30:09 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿    int numMoedas = 50;
+    bool[] moedas = new bool[numMoedas];
+
+    for (int i = 0; i < numMoedas; i++)
+    {
+        moedas[i] = true;
+    }
+
+    for (int pessoa = 1; pessoa <= numMoedas; pessoa++)
+    {
+        for (int moeda = pessoa - 1; moeda < numMoedas; moeda += pessoa)
+        {
+                moedas[moeda] = !moedas[moeda];
+        }
+    }
+
+    Console.WriteLine("Estado final das moedas:");
+    for (int i = 0; i < numMoedas; i++)
+    {
+            Console.WriteLine($"Moeda {i + 1}: {(moedas[i] ? "Cara" : "Coroa")}");
+    }
